3933b470253656b9363d4f2b340223ae44d739d9
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-04-09 11:53:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let arr = [1, 2, 3, 4, 5];
 function incrementArray(arr) {
   //your code here
+	const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArray.push(arr[i] + 1);
+  }
+  return newArray;
 }
 
 alert(incrementArray(arr));
